39002606c5ad6c7113cbf22c189f6e531027747b
kavunshiva <kavunshiva@gmail.com>
2017-05-30 16:07:37 -0400

    create complaints controller

--- rcf-api/config/routes.rb
+++ rcf-api/config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # resources :complaints, only: [:index]
+  # get '/api/v1/complaints', to: "complaints#index"
+  namespace :api do
+    namespace :v1 do
+      resources :complaints, only: :index
+    end
+  end
 end
