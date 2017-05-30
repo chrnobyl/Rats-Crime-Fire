5304ea75deae6018590c8811b4d801834f51c7a6
kavunshiva <kavunshiva@gmail.com>
2017-05-30 15:38:16 -0400

    create seeds

---
+++ rcf-api/db/seeds.rb
@@ -0,0 +1,30 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+require 'csv'
+
+filename = "db/rats.csv"
+
+read_file = File.read(filename)
+
+def format_to_datetime(time)
+  time.insert(5, "/"+time[0..1])[3..-1].to_datetime
+end
+
+File.foreach(filename) do |line|
+  # json_line = JSON.parse(line)
+  csv_line = CSV.parse(line)
+  if csv_line[0][1][6..9] == "2017" && Complaint.create(
+      complaint_type: csv_line[0][5],
+      time_of_complaint: format_to_datetime(csv_line[0][1]),
+      borough: csv_line[0][24],
+      zip_code: csv_line[0][8],
+      latitude: csv_line[0][50],
+      longitude: csv_line[0][51]
+    )
+  end
+end
