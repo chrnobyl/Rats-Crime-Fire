758300732922f7d2af9ec962ec498a078072ceea
github username <github email address>
2017-05-31 17:18:52 -0400

    google maps

--- js/index.js
+++ js/index.js
@@ -1,19 +1,37 @@
 $(document).ready(function(){
   console.log('Document ready!')
+  var map;
+  function initMap() {
+    map = $('#map')
+  }
+
   $('form#zip-code').on('submit', function(event){
     event.preventDefault()
     const zip = $('input[name = zip]').val()
     const url = `http://localhost:3000/api/v1/zip_codes/${zip}`
 
+
+
     $.ajax({
       url: url,
       success: function(data){
-        const lis = data.complaints.map(function(complaint){
-          return `<li>${complaint.date}</li>`
-          })
-          $('ul#complaints').html(lis.join(''))
-      }
 
+        data.complaints.map(function(complaint){
+          debugger
+          var coords = [complaint.latitude, complaint.longitude]
+          var latLng = new google.maps.LatLng(coords[0], coords[1])
+          var marker = new google.maps.Marker({
+            position: latLng,
+            map: map
+          })
+        })
+      }
     })
   })
 })
+
+
+
+          // return `<li>${complaint.date}</li>`
+          // })
+          // $('ul#complaints').html(lis.join(''))
