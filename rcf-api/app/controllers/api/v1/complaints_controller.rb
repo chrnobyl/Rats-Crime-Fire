167bb71fc1d08cf2bcaa6dfbe37b4c5f47105d6d
kavunshiva <kavunshiva@gmail.com>
2017-05-31 16:06:26 -0400

    implement crude complaints search

--- rcf-api/app/controllers/api/v1/complaints_controller.rb
+++ rcf-api/app/controllers/api/v1/complaints_controller.rb
@@ -11,4 +11,12 @@ class Api::V1::ComplaintsController < ApplicationController
     @complaint = Complaint.find_by(id: params[:id])
     render json: @complaint
   end
+
+  def search
+    if !params[:results]
+      params[:results] = 100
+    end
+    @complaints = Complaint.where("complaint_type ILIKE ?", params[:complaint_type]).order(:time_of_complaint)[0...params[:results].to_i]
+    render json: @complaints
+  end
 end
