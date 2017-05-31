d5097c341713fe5d36fc5d7731a100eedcdd6654
kavunshiva <kavunshiva@gmail.com>
2017-05-31 12:06:07 -0400

    only show data relevant to current view

--- rcf-api/app/controllers/api/v1/zip_codes_controller.rb
+++ rcf-api/app/controllers/api/v1/zip_codes_controller.rb
@@ -6,8 +6,7 @@ class Api::V1::ZipCodesController < ApplicationController
 
   def show
     @zip_code = ZipCode.find_by(number: params[:number])
-    render json: @zip_code
-
+    render json: @zip_code, serializer: ZipCodeShowSerializer
   end
 
 end
---
+++ rcf-api/app/serializers/zip_code_show_serializer.rb
@@ -0,0 +1,3 @@
+class ZipCodeShowSerializer < ZipCodeSerializer
+  has_many :complaints
+end
