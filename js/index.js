009ec432e44cf6cfb12f76e1cf595fde3f987356
github username <github email address>
2017-06-01 13:41:02 -0400

    GOOGLE MAP WORKS

--- js/index.js
+++ js/index.js
@@ -1,52 +1,54 @@
+var mapObj;
 $(document).ready(function(){
   console.log('Document ready!')
-  var mapObj;
+  setFormSubmitAction()
 
-  setTimeout(() => {
-    var marker = new google.maps.Marker({
-      position: {lat: -26, lng: 143},
-      map: mapObj
-    });
-  }, 2000);
+  // setTimeout(() => {
+  //
+  //   var marker = new google.maps.Marker({
+  //     position: {lat: -26, lng: 143},
+  //     map: mapObj
+  //   });
+  // }, 2000);
   // setFormSubmitAction()
+
 })
 
-const mapObj = function(map) {
-  debugger
-  return function(){
-    return map;
-  }
+function initMap() {
+  var nyc = {lat: 40.7255944265592, lng: -73.9446377360189};
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 10,
+    center: nyc
+  });
+  mapObj = map
+
 }
 
-mapObj(theMapFromInitMap);
-
-mapObj() // returns theMapFromInitMap
-
-function initMap() {
-  var uluru = {lat: -25.363, lng: 131.044};
-  var map = new google.maps.Map(document.getElementById('map'), {
-    zoom: 4,
-    center: uluru
-  });
+function makeNewMarker(position){
   var marker = new google.maps.Marker({
-    position: {lat: -27, lng: 145},
-    map: map
-  });
-  mapObj(map);
+    position: position,
+    map: mapObj
+  })
 }
 
 function setFormSubmitAction(){
   $('form#zip-code').on('submit', function(event){
     event.preventDefault()
     const zip = $('input[name = zip]').val()
-    const url = `http://localhost:3000/api/v1/zip_codes/${zip}`
+    // const url = `http://localhost:3000/api/v1/zip_codes/${zip}`
+    const url = `http://localhost:3000/api/v1/complaints/search?zip_code=${zip}&complaint_type=rodent&description=rat+sighting`
 
     $.ajax({
       url: url,
-      data: data,
       success: function(data){
         console.log("hello!")
+        data.map(function(complaint){
+          var position = {lat: complaint.latitude, lng: complaint.longitude}
+          makeNewMarker(position)
+        })
         // var nyc = {lat: 40.7255944265592, lng: -73.9446377360189}
+
+
       }
 
       // function(data){
