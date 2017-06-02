6a66bb6226f719fbb2982554a2a3ba5a2f402f7e
github username <github email address>
2017-06-02 11:32:56 -0400

    buttons on stats

--- js/stats.js
+++ js/stats.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-  $('form#total_complaints').on('submit', function(event){
+  $('#butt1').on('click', function(event){
     event.preventDefault()
     const guess = $('input[name=tcomplaints]').val()
     $.ajax({
@@ -19,7 +19,7 @@ $(document).ready(function(){
       }
     })
   })
-  $('#submitButton').on('click', function(event){
+  $('#butt2').on('click', function(event){
     event.preventDefault()
     const guess2 = $('input[name=bcomplaints]').val()
     $.ajax({
@@ -83,37 +83,37 @@ $(document).ready(function(){
       })
   })
 
-  $('#submitButtonH').on('click', function(event){
+  $('#butt3').on('click', function(event){
     event.preventDefault()
     $('#ansH').html ('<h5> "Pidgeon Odor"  </h5>')
     $('#ansHPic').html("<img src= 'https://cdn.shopify.com/s/files/1/1028/1501/files/62e104a5ebd8272209d6bc3ff7a3f0d6_grande.jpg?12701140460794891227' alt='sup' height='200' width='400'>")
   })
 
-  $('#submitButton4').on('click', function(event){
+  $('#butt4').on('click', function(event){
     event.preventDefault()
     $('#ans4Desc').html ('<h5> "Iguana" </h5>')
     $('#ans4Pic').html("<img src= 'https://ak6.picdn.net/shutterstock/videos/6042848/thumb/4.jpg' height='200' width='400'>")
   })
 
-  $('#submitButton5').on('click', function(event){
+  $('#butt5').on('click', function(event){
     event.preventDefault()
     $('#ans5Desc').html ('<h5> "Bees/Wasps - Not Beekeeper" </h5>')
     $('#ans5Pic').html("<img src= 'http://ucanr.edu/blogs/bugsquad//blogfiles/27724_original.jpg' >")
   })
 
-  $('#submitButton6').on('click', function(event){
+  $('#butt6').on('click', function(event){
     event.preventDefault()
     $('#ans6Desc').html ('<h5> "Unpasteurized Milk" </h5>')
     $('#ans6Pic').html("<img src= 'http://cdn.thealternativedaily.com/wp-content/uploads/2016/08/Goat-milking.jpg'>")
   })
 
-  $('#submitButton7').on('click', function(event){
+  $('#butt7').on('click', function(event){
     event.preventDefault()
     $('#ans7Desc').html ('<h5> "Turtle Under 4 Inches Long" </h5>')
     $('#ans7Pic').html("<img src= 'http://cutepics.org/wp-content/uploads/2011/10/small-turtle.jpg'>")
   })
 
-  $('#submitButton8').on('click', function(event){
+  $('#butt8').on('click', function(event){
     event.preventDefault()
     $('#ans8Desc').html ('<h5> "Minor Recieved Tattoo" </h5>')
     $('#ans8Pic').html("<img src= 'http://tattoospedia.com/deepsearches/Tattoo%20Fail/Kid%20Tattoo%20Fail%201.jpg'>")
